ec0cb113719dd6f1fc0115b293ee4a89c5197915
KyeongzowD <109902932+KyeongzowD@users.noreply.github.com>
2022-07-31 19:29:20 +0900

    [level 1] Title: 폰켓몬, Time: 2.43 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 1;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]!=nums[i+1]){
+                answer++;
+            }
+        }
+        if(nums.length/2<answer){
+            answer=nums.length/2;
+        }
+        return answer;
+    } 
+}
