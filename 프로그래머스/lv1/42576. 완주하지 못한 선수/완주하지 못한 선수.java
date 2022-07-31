c97ff775e8e49fffedf26df3a18f2c941f3478e5
KyeongzowD <109902932+KyeongzowD@users.noreply.github.com>
2022-07-31 20:21:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 319.36 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,19 +3,15 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Arrays.sort(participant);
-        System.out.println();
         Arrays.sort(completion);
-        for(int i=0;i<completion.length;i++)
-        {
-            if(participant[i].equals(completion[i])){
-                
-            }else{
+        for(int i=0;i<completion.length;i++){
+            if(participant[i].equals(completion[i])){}
+            else{
                 answer= participant[i];
                 break;
             }                
         }
-        if(answer.equals(""))
-        {
+        if(answer.equals("")){
             answer=participant[participant.length-1];
         }
         return answer;
