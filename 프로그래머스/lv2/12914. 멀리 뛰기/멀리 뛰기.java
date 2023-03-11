2d4af26553104e951c81ba9e26c4bcf68d6f1488
KyeongzowD <109902932+KyeongzowD@users.noreply.github.com>
2023-03-11 21:49:58 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.15 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        long[] arr=new long[2000];
+        arr[0]=1;
+        arr[1]=2;
+        
+        for(int i=2;i<n;i++){
+            arr[i]=(arr[i-1]+arr[i-2])%1234567;
+        }
+        
+        answer=arr[n-1]%1234567;
+        
+        return answer;
+    }
+}
