e1dbd67fbd585b94baa1b1ec0a7b3670d1302035
김경은 <109902932+KyeongzowD@users.noreply.github.com>
2024-05-21 10:15:51 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,27 +1,22 @@
 import java.util.Scanner;
 public class Main
 {
-    public static void main(String[] args)
-    {
-       Scanner sc= new Scanner(System.in);
-       int[] arr=new int[9];
-       for(int i=0;i<9;i++)
-       {
-            arr[i]=sc.nextInt();
-       }
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int []arr = new int[9]; 
+      for(int i=0;i<9;i++){
+         arr[i] = sc.nextInt(); 
+      }
+       sc.close();
        int max=arr[0];
-       int count=1;
-       for(int i=1;i<9;i++)
-       {
-           if(max<arr[i])
-               max=arr[i];
-       }
-        for(int i=0;i<9;i++)
-        {
-            if(max==arr[i])
-                break;
-            count++;
-        }
-       System.out.println(max+"\n"+count);
-    }
+       int num=1;
+       for(int j=1;j<9;j++){
+         if(max<arr[j]){
+             max = arr[j];
+             num = j+1;
+         }
+      }
+      System.out.println(max);
+      System.out.println(num);
+   }
 }
