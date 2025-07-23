0a5397fe0504b22a75c9a418b2f21aae3f3c9a6e
김경은 <109902932+KyeongzowD@users.noreply.github.com>
2025-07-23 22:23:31 +0900

    [Bronze I] Title: 약수, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        bw.write(String.valueOf(arr[0]*arr[N-1]));
+        bw.flush();
+    }
+}
